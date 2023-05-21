d0adf43f1ca8e3fc89acdb91d8ccaf4d45ac1c08
tajammul1996 <tpzuffari@gmail.com>
2023-05-21 19:18:26 +0530

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,13 @@
 const nextConfig = {
     images: {
         // allow all domains
-        domains: ["img5a.flixcart.com", "img6a.flixcart.com"],
+        // domains: ["img5a.flixcart.com", "img6a.flixcart.com"],
         remotePatterns: [{
             protocol: 'http',
-            hostname: "**",
-            pathname: "/**",
+            hostname: "img5a.flixcart.com",
+        }, {
+            protocol: 'http',
+            hostname: "img6a.flixcart.com",
         }]
     },
     redirects: async () => {
